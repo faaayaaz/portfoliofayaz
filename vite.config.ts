563f9b120ab89cf2bf46d0e9f4b2020f32b231d5
faaayaaz <78535017+faaayaaz@users.noreply.github.com>
2025-04-21 13:58:19 +0400

    chore: enable production sourcemaps for debugging

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -7,14 +6,16 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/portfoliofayaz/' : '/', // Set base path for GitHub Pages
+  build: {
+    sourcemap: true, // <<< ADD THIS
+  },
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
